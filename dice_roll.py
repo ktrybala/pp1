e9917477aab9088db98b65d32689f9f646da18bd
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-10-18 22:56:34 +0200

    dice roll programm

---
+++ dice_roll.py
@@ -0,0 +1,14 @@
+import random
+roll_amount = int(input("How many times you want to roll the dice: "))
+
+#wartosci zmiennych
+y = 1
+sum = 0
+
+#rzut kostką
+while y <= roll_amount:  
+   x = random.randint(1,6)
+   sum += x
+   y += 1
+   
+print(sum)
