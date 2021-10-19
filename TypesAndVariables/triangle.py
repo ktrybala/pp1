2f49d56fa24d47da059d156a44d4f0f622a8df0a
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-10-19 14:45:59 +0200

    changes

--- TypesAndVariables/triangle.py
+++ TypesAndVariables/triangle.py
@@ -7,12 +7,12 @@ c = int(input("Value of the third side: "))
 sides = [a,b,c]
 sides.sort() #sorting sides by theirs value
 
-def haron():
+def heron():
 	p = (a + b + c) / 2
 	area = math.sqrt(p * (p - a) * (p - b) * (p - c))
 	print(f"Area of the triangle: {round(area,2)}")
 
 if (sides[0] + sides[1]) > sides[2]:
-	haron()
+	heron()
 else:
 	print("Change values of triangle sides, they do not meet the criterias of triangle inequlity.")
