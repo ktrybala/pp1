a07060062feaf7154fe10d882ddf1fc5c3c01b6b
Kacper Trybała <ktrybala@macbook-ktrybala.home>
2021-10-20 00:13:23 +0200

    sample

---
+++ personal/photo_test.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+main = Tk()
+
+def open_img():
+    global img
+    path = r"/users/ktrybala/documents/bobo.png"
+    img = ImageTk.PhotoImage(Image.open(path))
+    panel = Label(main, image = img)
+    panel.pack()
+but1 = Button(main, text="click to get the image", command=open_img)
+but1.pack()
+main.mainloop()
