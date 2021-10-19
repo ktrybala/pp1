b1306a97ae3545a6bef4904aa43dcebc179a920e
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-10-19 14:44:12 +0200

    heron formula

---
+++ TypesAndVariables/triangle.py
@@ -0,0 +1,18 @@
+import math
+
+a = int(input("Value of the first side: "))
+b = int(input("Value of the second side: "))
+c = int(input("Value of the third side: "))
+
+sides = [a,b,c]
+sides.sort() #sorting sides by theirs value
+
+def haron():
+	p = (a + b + c) / 2
+	area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+	print(f"Area of the triangle: {round(area,2)}")
+
+if (sides[0] + sides[1]) > sides[2]:
+	haron()
+else:
+	print("Change values of triangle sides, they do not meet the criterias of triangle inequlity.")
