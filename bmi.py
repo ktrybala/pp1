49ec20d4a6098a7886236e1ed715b8e3adbd2f80
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-10-19 13:41:47 +0200

    changes

--- bmi.py
+++
@@ -1,7 +0,0 @@
-import math 
-height = int(input("Enter your height in cm: "))
-weight = int(input("Enter your weight in kg: "))
-sqr_height = (height/100)**2
-bmi = (weight)/sqr_height
-#wynik bmi zostanie zaokragony do setnych
-print(f"Your BMI is: {round(bmi,2)} ")
