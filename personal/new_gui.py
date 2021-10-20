850396ee5e495d01fb773e72ff4420ce0dbaa1e5
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-10-20 09:36:33 +0200

    changes

---
+++ personal/new_gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+main = Tk()
+main.geometry("500x500")
+main.title('tak koń')
+
+button = Button(main, text = 'exit', highlightbackground = 'red', fg='white', command = main.destroy)
+button.place(relx=0.5, rely=0.01, anchor=N)
+#button.pack()
+text_x = Label(main, text = "Number x: ").place(relx = 0.15, rely= 0.5, anchor=W)
+wpis = StringVar()
+x = Entry(main, textvariable = wpis)
+wpis = x.get()
+x.place(relx=0.5, rely=0.5, anchor=CENTER)
+main.mainloop()
+print(wpis)
