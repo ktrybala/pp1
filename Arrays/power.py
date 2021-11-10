132a5ab2e8ee6dba21ebf2a6fe1b2f81db75a4fd
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2021-11-10 19:39:18 +0100

    power

---
+++ Arrays/power.py
@@ -0,0 +1,15 @@
+tab = [8, 2, 5, 1, 9]
+tab2 = []
+
+for i in tab:
+	tab2.append(i**2)
+
+print("Array: ", end = "")
+for x in tab:
+	print(x," ", end = "")
+
+print("")
+print("2nd Power: ", end = "")
+for x in tab2:
+	print(x," ", end = "")
+print("")
