cbefc8f6fddef819177b53dd8f336594dfd0a483
Kacper Trybała <ktrybala@macbook-ktrybala.home>
2021-11-30 22:27:46 +0100

    done

--- mock2/F6.py
+++ mock2/F6.py
@@ -1,1 +1,12 @@
+import csv
 def f6(g,n1,n2):
+	x = 0
+	with open("people.csv","r") as f:
+		content = csv.reader(f)
+		next(content)
+		for row in content:
+			h = int(row[2])
+			if row[3] == g and n1 <= h <= n2:
+				x += 1
+	return x
+print(f6("Female",160,180))
